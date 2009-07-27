ccde6fd47c5e4a1ea624478648887332cd8cfd49
Christoph Höger <choeger@cs.tu-berlin.de>
2009-07-27 20:22:39 +0200

    launch configuration work started
    * added all basic classes for launch configurations
    * added launch dependencies
    * added TextButtongroup for easier dialogs
    * added basic main.cpp template for the IDA solver
    
    * TODO: + make launch work (figure out File IO stuff)
    	+ choose solvers dynamically
    	+ add solver tabs
    	+ think of selecting
    	solver per radio button in own tab
    
    Signed-off-by: Christoph Höger <choeger@cs.tu-berlin.de>

---
+++ src/main/java/de/tuberlin/uebb/emodelica/launch/LaunchShortcut.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package de.tuberlin.uebb.emodelica.launch;
+
+import org.eclipse.debug.ui.ILaunchShortcut;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IEditorPart;
+
+/**
+ * @author choeger
+ *
+ */
+public class LaunchShortcut implements ILaunchShortcut {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.debug.ui.ILaunchShortcut#launch(org.eclipse.jface.viewers.ISelection, java.lang.String)
+	 */
+	@Override
+	public void launch(ISelection selection, String mode) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.debug.ui.ILaunchShortcut#launch(org.eclipse.ui.IEditorPart, java.lang.String)
+	 */
+	@Override
+	public void launch(IEditorPart editor, String mode) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
