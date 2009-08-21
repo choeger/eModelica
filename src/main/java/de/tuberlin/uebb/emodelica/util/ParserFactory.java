cd478c0bb1a4e8ffdc6a1bcea41e98ddea048792
Christoph Höger <choeger@cs.tu-berlin.de>
2009-08-21 23:50:27 +0200

    fix setup to match im-0.0.3
    Signed-off-by: Christoph Höger <choeger@cs.tu-berlin.de>

--- src/main/java/de/tuberlin/uebb/emodelica/util/ParserFactory.java
+++ src/main/java/de/tuberlin/uebb/emodelica/util/ParserFactory.java
@@ -5,7 +5,7 @@ package de.tuberlin.uebb.emodelica.util;
 
 import java.util.ArrayDeque;
 
-import de.tuberlin.uebb.modelica.im.generated.moparser.modelicaStateFactory;
+import de.tuberlin.uebb.modelica.im.generated.moparser.mosilaStateFactory;
 import de.tuberlin.uebb.page.parser.Automaton;
 
 /**
@@ -16,11 +16,11 @@ public class ParserFactory {
 	static class ParserConstructor implements Runnable {
 
 		private ArrayDeque<Automaton> parserQue;
-		private modelicaStateFactory stateFactory;
+		private mosilaStateFactory stateFactory;
 
 		public ParserConstructor(ArrayDeque<Automaton> parser) {
 			this.parserQue = parser;
-			stateFactory = new modelicaStateFactory();
+			stateFactory = new mosilaStateFactory();
 		}
 
 		@Override
